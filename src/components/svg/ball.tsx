100d1016109194ea1754c24c1c35c7845b148db0
nera1 <nera4936@gmail.com>
2025-01-11 20:47:04 +0900

    feature: add BallSVGProps interface

--- src/components/svg/ball.tsx
+++ src/components/svg/ball.tsx
@@ -1,5 +1,12 @@
-import { FunctionComponent } from "react";
-const Ball: FunctionComponent<any> = ({ className, ...props }) => (
+import { FunctionComponent, SVGProps } from "react";
+
+interface CustomSVGProps extends SVGProps<SVGSVGElement> {
+  className?: string;
+  width?: number;
+  height?: number;
+}
+
+const Ball: FunctionComponent<CustomSVGProps> = ({ className, ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
