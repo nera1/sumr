51c8e10c4bd5fc588d6def45318a1c780715915a
nera1 <nera4936@gmail.com>
2025-11-03 23:50:09 +0900

    chore: update posts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Sumr',
+    description: `흩어져 사라지기 쉬운 작은 지식과 경험들을 놓치지 않기 위해, 기록하며 붙잡아 보려 애쓰는 개발자의 블로그입니다.`,
+    imgSrc: '/static/imgs/1/2.png',
+    href: 'https://sumr.it',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Madness',
+    description: `어제도, 작년에도, 심지어 더 옛날에도 봤던 거 말고, 처음보는 것들을 위한 커뮤니티`,
+    imgSrc: '/static/images/madness.svg',
+    href: '/blog/manual',
   },
 ]
 
