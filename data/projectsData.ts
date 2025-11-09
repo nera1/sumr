23de8e8d9a7d8b9dc1065fffd814eecf537a9721
nera1 <nera4936@gmail.com>
2025-11-09 22:43:13 +0900

    docs: update my project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,15 +8,15 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'Sumr',
-    description: `흩어져 사라지기 쉬운 작은 지식과 경험들을 놓치지 않기 위해, 기록하며 붙잡아 보려 애쓰는 개발자의 블로그입니다.`,
+    description: `흩어져 사라지기 쉬운 작은 지식과 경험들을 놓치지 않기 위해, 기록하며 붙잡아 보려 애쓰는 개발자의 블로그입니다`,
     imgSrc: '/static/imgs/1/2.png',
     href: 'https://sumr.it',
   },
   {
     title: 'Madness',
-    description: `어제도, 작년에도, 심지어 더 옛날에도 봤던 거 말고, 처음보는 것들을 위한 커뮤니티`,
+    description: `온라인 프레젠테이션 서비스, Notion의 프레젠테이션 버전`,
     imgSrc: '/static/images/madness.svg',
-    href: '/blog/manual',
+    href: 'https://madn.es',
   },
 ]
 
